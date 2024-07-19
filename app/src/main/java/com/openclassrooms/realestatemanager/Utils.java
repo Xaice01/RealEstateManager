667807294dd1e91c267b7742fa1805ee9ce3177d
Xaice01 <103635912+Xaice01@users.noreply.github.com>
2024-07-19 16:01:55 +0200

    correct error in MainActivity.java, add convertEuroToDollar and change isInternetAvailable in Utils.java.add test NetworkUtilsTest.java and UtilsTest.java

--- app/src/main/java/com/openclassrooms/realestatemanager/Utils.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/Utils.java
@@ -1,7 +1,11 @@
 package com.openclassrooms.realestatemanager;
 
 import android.content.Context;
-import android.net.wifi.WifiManager;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkCapabilities;
+import android.net.NetworkInfo;
+import android.os.Build;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -13,6 +17,8 @@ import java.util.Date;
 
 public class Utils {
 
+    static Double ExchangeDollarsEuros = 0.812;
+
     /**
      * Conversion d'un prix d'un bien immobilier (Dollars vers Euros)
      * NOTE : NE PAS SUPPRIMER, A MONTRER DURANT LA SOUTENANCE
@@ -20,7 +26,17 @@ public class Utils {
      * @return
      */
     public static int convertDollarToEuro(int dollars){
-        return (int) Math.round(dollars * 0.812);
+        return (int) Math.round(dollars * ExchangeDollarsEuros);
+    }
+
+    /**
+     * Conversion d'un prix d'un bien immobilier (Euros vers Dollars)
+     * NOTE : NE PAS SUPPRIMER, A MONTRER DURANT LA SOUTENANCE
+     * @param euros
+     * @return
+     */
+    public static int convertEuroToDollar(int euros){
+        return (int) Math.round(euros / ExchangeDollarsEuros);
     }
 
     /**
@@ -29,7 +45,7 @@ public class Utils {
      * @return
      */
     public static String getTodayDate(){
-        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
         return dateFormat.format(new Date());
     }
 
@@ -40,7 +56,25 @@ public class Utils {
      * @return
      */
     public static Boolean isInternetAvailable(Context context){
-        WifiManager wifi = (WifiManager)context.getSystemService(Context.WIFI_SERVICE);
-        return wifi.isWifiEnabled();
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivityManager != null) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                Network network = connectivityManager.getActiveNetwork();
+                if (network == null) {
+                    return false;
+                }
+                NetworkCapabilities activeNetwork = connectivityManager.getNetworkCapabilities(network);
+                if (activeNetwork == null) {
+                    return false;
+                }
+                return activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)
+                        || activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)
+                        || activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET);
+            } else {
+                NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+                return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+            }
+        }
+        return false;
     }
 }
