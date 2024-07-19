1b55010b9099888420bed7fc7377b4296f15f415
Xaice01 <103635912+Xaice01@users.noreply.github.com>
2024-07-19 15:10:03 +0200

    Create

---
+++ app/src/androidTest/java/com/openclassrooms/realestatemanager/NetworkUtilsTest.java
@@ -0,0 +1,23 @@
+package com.openclassrooms.realestatemanager;
+import android.content.Context;
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+
+@RunWith(AndroidJUnit4.class)
+public class NetworkUtilsTest {
+
+    @Test
+    public void testInternetAvailable() {
+        Context context = ApplicationProvider.getApplicationContext();
+
+        // Assuming the device/emulator has internet connection
+        boolean isConnected = Utils.isInternetAvailable(context);
+        assertTrue("Device should be connected to the internet", isConnected);
+    }
+
+}
